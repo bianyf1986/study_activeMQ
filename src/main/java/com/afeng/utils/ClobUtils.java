4476ecbb6f769a6f3e9ca834d67037396a914dc7
bianyf1986 <416352221@qq.com>
2016-12-28 18:15:03 +0800

    【工具类】【 20161228】添加文件处理、ip、加密工具类

---
+++ src/main/java/com/afeng/utils/ClobUtils.java
@@ -0,0 +1,67 @@
+package com.afeng.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javax.sql.rowset.serial.SerialClob;
+import javax.sql.rowset.serial.SerialException;
+
+/**
+ * 
+ * @Description : java.sql.Clob类型工具类
+ * @author      : yfbian
+ */
+public class ClobUtils {
+
+	/**
+	 * 
+	 * @Function :  getString
+	 * @Desc     :  Clob类型to字符串类型
+	 * @Author   :  yfbian
+	 * @param clob java.sql.Clob类型
+	 * @return String类型数据  
+	 */
+	public static String getString(java.sql.Clob clob) {
+		StringBuffer sb = new StringBuffer();
+		if (clob != null) {
+			try {
+				BufferedReader bufferReader = new BufferedReader(clob.getCharacterStream());
+				try {
+					String str;
+					while ((str = bufferReader.readLine()) != null) {
+						sb.append(str);
+					}
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return sb.toString();
+	}
+
+	/**
+	 * 
+	 * @Function :  getClob
+	 * @Desc     :  字符串类型toClob类型
+	 * @Author   :  yfbian
+	 * @param str String类型
+	 * @return java.sql.Clob类型数据  
+	 */
+	public static java.sql.Clob getClob(String str) {
+		java.sql.Clob clob = null;
+		try {
+			if (str != null) {
+				clob = new SerialClob(str.toCharArray());
+			}
+		} catch (SerialException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return clob;
+	}
+
+}
---
+++ src/main/java/com/afeng/utils/EncryptUtils.java
@@ -0,0 +1,99 @@
+package com.afeng.utils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 
+ * @Description : 加密工具类：md5加密出来的长度是32位、sha加密出来的长度是40位
+ * @author      : yfbian
+ */
+public class EncryptUtils {
+	
+	public static void main(String[] args) {
+		System.out.println(md5AndSha("12313"));
+		System.out.println(md5AndSha("12313").length());
+	}
+
+	/**
+	 * 
+	 * @Function :  md5
+	 * @Desc     :  md5加密
+	 * @Author   :  yfbian
+	 * @param inputText 加密目标串
+	 * @return 加密结果串
+	 */
+	public static String md5(String inputText) {
+		return encrypt(inputText, "md5");
+	}
+	
+	/**
+	 * 
+	 * @Function :  sha
+	 * @Desc     :  sha加密
+	 * @Author   :  yfbian
+	 * @param inputText 加密目标串
+	 * @return 加密结果串
+	 */
+	public static String sha(String inputText) {
+		return encrypt(inputText, "sha-1");
+	}
+	
+	/**
+	 * 
+	 * @Function :  md5AndSha
+	 * @Desc     :  md5和sha双加密
+	 * @Author   :  yfbian
+	 * @param inputText 加密目标串
+	 * @return 加密结果串
+	 */
+	public static String md5AndSha(String inputText) {
+		return sha(md5(inputText));
+	}
+
+	/**
+	 * 
+	 * @Function :  encrypt
+	 * @Desc     :  md5或者sha-1加密
+	 * @Author   :  yfbian
+	 * @param inputText 要加密的内容
+	 * @param algorithmName 加密算法名称：md5或者sha-1，不区分大小写
+	 * @return 加密结果串
+	 */
+	private static String encrypt(String inputText, String algorithmName) {
+		if (inputText == null || "".equals(inputText.trim())) {
+			throw new IllegalArgumentException("请输入要加密的内容");
+		}
+		if (algorithmName == null || "".equals(algorithmName.trim())) {
+			algorithmName = "md5";
+		}
+		String encryptText = null;
+		try {
+			MessageDigest m = MessageDigest.getInstance(algorithmName);
+			m.update(inputText.getBytes("UTF8"));
+			byte s[] = m.digest();
+			return hex(s);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return encryptText;
+	}
+
+	/**
+	 * 
+	 * @Desc     :  返回十六进制字符串
+	 * @param arr byte[]
+	 * @return 十六进制字符串
+	 */
+	private static String hex(byte[] arr) {
+		StringBuffer sb = new StringBuffer();
+		for (int i = 0; i < arr.length; ++i) {
+			sb.append(Integer.toHexString((arr[i] & 0xFF) | 0x100).substring(1, 3));
+		}
+		return sb.toString();
+	}
+	
+}
