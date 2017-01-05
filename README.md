9260a0abea6f6e82fb7c4ce7a61d690b1f8e63b8
bianyf1986 <416352221@qq.com>
2017-01-05 15:10:59 +0800

    学习ThreadLocal

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 String、File、IP、Encrypt加密、Clob
 # 配置文件工具类
 1、包路径：com.afeng.utils.loadConfig。  2、PropertyResourceBundle工具。  3、spring配置文件。  4、spring注解。
-# 多线程工具类
-ThreadPoolExecutor框架
+# 多线程
+ThreadPoolExecutor框架、线程中断interrupted、join、sleep、countDownLatch、ThreadLocal
---
+++ src/main/java/com/afeng/utils/multiThread/threadLocal/UnsafeTaskMain.java
@@ -0,0 +1,24 @@
+package com.afeng.utils.multiThread.threadLocal;
+
+import java.util.concurrent.TimeUnit;
+
+public class UnsafeTaskMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		UnsafeTask task = new UnsafeTask();  
+        for(int i = 0; i<10;i++){  
+            Thread thread = new Thread(task);  
+            thread.start();  
+              
+            try {  
+                TimeUnit.SECONDS.sleep(2);  
+            } catch (InterruptedException e) {  
+                e.printStackTrace();  
+            }  
+        } 
+	}
+
+}
