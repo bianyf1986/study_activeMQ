48dece42d25871db09aea7f6d6f29e6e50540358
bianyf1986 <416352221@qq.com>
2017-01-05 22:41:32 +0800

    梳理join方法的使用

--- src/main/java/com/afeng/utils/multiThread/join/JoinDemo.java
+++ src/main/java/com/afeng/utils/multiThread/join/JoinDemo.java
@@ -1,17 +1,18 @@
 package com.afeng.utils.multiThread.join;
 
-import java.util.Date;
-
 public class JoinDemo implements Runnable{
 
 	@Override
 	public void run() {
+		
+		System.out.println("线程："+Thread.currentThread().getName()+",开始执行！");
+		
 		for(int i=0;i<10;i++){
-			System.out.println("i="+i);  
+			System.out.println("线程："+Thread.currentThread().getName()+",执行过程 "+i);
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
-				System.out.println("The JoinDemo has been interrupted");  
+				System.out.println("线程："+Thread.currentThread().getName()+",has been interrupted");
 			}
 			
 		}
--- src/main/java/com/afeng/utils/multiThread/join/JoinTest.java
+++ src/main/java/com/afeng/utils/multiThread/join/JoinTest.java
@@ -8,18 +8,25 @@ public class JoinTest {
 	public static void main(String[] args) {
 		JoinDemo demo = new JoinDemo();
 		Thread thread = new Thread(demo);
-		thread.start();
-		
-		//demo.notify();
-		System.out.println("1111111");
+		thread.start();//启动子线程
 		
 		try {
-			thread.join();
+			System.out.println("线程："+Thread.currentThread().getName()+",开始睡眠！");
+			Thread.sleep(2000);//主线程main睡2s，执行时间让给子线程执行2s
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		
+		System.out.println("线程："+Thread.currentThread().getName()+",睡眠结束！");
+		
+		try {
+			System.out.println("线程："+Thread.currentThread().getName()+",等待线程："+thread.getName()+" 执行完毕！");
+			thread.join();//主线程main等待子线程thread执行完毕后，才会继续执行
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("2222222");
+		System.out.println("线程："+Thread.currentThread().getName()+",即将结束！");
 		
 	}
 
